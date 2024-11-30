1db1f1b1b4fbab3d8febff26a7c4d9e234e6ef25
İbrahim KAYA <121380865+berkayslc@users.noreply.github.com>
2024-11-30 18:34:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,5 @@ if st.button("Tahmin Et"):
     predicted_salary_f_2024 = model.predict(input_data)[0]*2.06  # Modelin döndürdüğü ilk (ve tek) tahmin sonucu 2024 için 2.06 ile çarpılmıştır. Enflasyon hesaba katıldı.
 
     # Kullanıcıya tahmini maaşı gösteriyoruz
-    st.write(f"2023 Tahmini Maaş: **{predicted_salary_f_2022:,.2f} TL**")  # Maaşı düzgün formatta (ör. 1.000,00 TL) gösteriyoruz
+    st.write(f"2022 Tahmini Maaş: **{predicted_salary_f_2022:,.2f} TL**")  # Maaşı düzgün formatta (ör. 1.000,00 TL) gösteriyoruz
     st.write(f"2024 Tahmini Maaş: **{predicted_salary_f_2024:,.2f} TL**")  # Maaşı düzgün formatta (ör. 1.000,00 TL) gösteriyoruz
